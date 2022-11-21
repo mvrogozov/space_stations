91dd59cef13eb5db1fe7208c6cdc302527e78f8d
Рогозов Михаил <mv.rogozov@yandex.ru>
2022-11-21 10:59:04 +0300

    models add

--- space_stations/space/stations/admin.py
+++ space_stations/space/stations/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
+from .models import Station, Coordinates, Command
+
+
+class CoordinatesInlines(admin.StackedInline):
+    model = Coordinates
 
 
 class StationsAdmin(admin.ModelAdmin):
-    pass
+    inlines = [CoordinatesInlines]
 
+    list_display = (
+        'name',
+        'status',
+        'create_date',
+        'brake_date',
+        'coordinates'
+    )
+
+
+admin.site.register(Station, StationsAdmin)
+admin.site.register(Coordinates)
+admin.site.register(Command)
--- space_stations/space/stations/models.py
+++ space_stations/space/stations/models.py
@@ -1,3 +1,78 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Station(models.Model):
+    CHOICES = (
+        ('OK', 'running'),
+        ('BR', 'broken')
+    )
+
+    name = models.CharField(
+        'Имя',
+        max_length=254,
+        unique=True,
+        null=False
+    )
+    status = models.CharField(
+        max_length=32,
+        choices=CHOICES,
+        verbose_name='Статус'
+    )
+    create_date = models.DateTimeField(
+        'Дата создания',
+        auto_now_add=True
+    )
+    brake_date = models.DateTimeField(
+        'Дата поломки',
+        null=True,
+        blank=True
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Coordinates(models.Model):
+    x = models.IntegerField(default=100)
+    y = models.IntegerField(default=100)
+    z = models.IntegerField(default=100)
+    station = models.OneToOneField(
+        Station,
+        on_delete=models.CASCADE,
+        related_name='coordinates',
+        verbose_name='Станция'
+    )
+
+    def __str__(self):
+        return self.station.name
+
+
+class Command(models.Model):
+    CHOICES = (
+        ('X', 'X'),
+        ('Y', 'Y'),
+        ('Z', 'Z')
+    )
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Пользователь'
+    )
+    axis = models.CharField(
+        choices=CHOICES,
+        max_length=8,
+        verbose_name='Ось'
+    )
+    distance = models.IntegerField(
+        'Расстояние'
+    )
+    station = models.ForeignKey(
+        Station,
+        on_delete=models.CASCADE,
+        verbose_name='Станция'
+    )
+
+    def __str__(self):
+        return f'{self.user} - {self.station.name}'
